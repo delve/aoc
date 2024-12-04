3b44fb0db2fd5c7b95a2b3d115c73e62e2579ac1
delve <delve202@gmail.com>
2024-12-04 08:26:39 +0000

    solved 2024/04

--- pkg/year2024/day04.go
+++ pkg/year2024/day04.go
@@ -1,5 +1,9 @@
 package year2024
 
+import (
+	"math"
+)
+
 type Day04 struct{}
 
 func row(c complex128) int {
@@ -10,11 +14,44 @@ func column(c complex128) int {
 	return int(imag(c))
 }
 
+// length and bounds are one-based, position is xero-based
+func checkBounds(direction rune, position complex128, length int, bounds complex128) bool {
+	// 0-base the inputs
+	ln := length - 1
+	height := row(bounds) - 1
+	width := column(bounds) - 1
+	if direction == 'U' {
+		if row(position) < ln {
+			// not enough room up
+			return false
+		}
+	}
+	if direction == 'D' {
+		if row(position) > height-ln {
+			// not enough room down
+			return false
+		}
+	}
+	if direction == 'L' {
+		if column(position) < ln {
+			// not enough room left
+			return false
+		}
+	}
+	if direction == 'R' {
+		if column(position) > width-ln {
+			// not enough room right
+			return false
+		}
+	}
+
+	return true
+}
+
 // clock shaped directions
 func wordSearchUp(input []string, start complex128, keyword string) bool {
 	// bounds checking
-	if row(start) < len(keyword)-1 {
-		// not enough room for vertical xmas
+	if !checkBounds('U', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
 		return false
 	}
 
@@ -28,8 +65,90 @@ func wordSearchUp(input []string, start complex128, keyword string) bool {
 
 func wordSearchRight(input []string, start complex128, keyword string) bool {
 	// bounds checking
-	if column(start) > len(input[row(start)])-len(keyword) {
-		// not enough room for horizontal xmas
+	if !checkBounds('R', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	for idx, letter := range keyword {
+		if input[row(start)][column(start)+idx] != byte(letter) {
+			return false
+		}
+	}
+	return true
+}
+
+func wordSearchUpRight(input []string, start complex128, keyword string) bool {
+	// bounds checking
+	if !checkBounds('U', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('R', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	for idx, letter := range keyword {
+		if input[row(start)-idx][column(start)+idx] != byte(letter) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func wordSearchDownRight(input []string, start complex128, keyword string) bool {
+	// bounds checking
+	if !checkBounds('D', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('R', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	for idx, letter := range keyword {
+		if input[row(start)+idx][column(start)+idx] != byte(letter) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func wordSearchDown(input []string, start complex128, keyword string) bool {
+	// bounds checking
+	if !checkBounds('D', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	for idx, letter := range keyword {
+		if input[row(start)+idx][column(start)] != byte(letter) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func wordSearchDownLeft(input []string, start complex128, keyword string) bool {
+	// bounds checking
+	if !checkBounds('D', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('L', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	for idx, letter := range keyword {
+		if input[row(start)+idx][column(start)-idx] != byte(letter) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func wordSearchLeft(input []string, start complex128, keyword string) bool {
+	// bounds checking
+	if !checkBounds('L', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
 		return false
 	}
 
@@ -38,31 +157,61 @@ func wordSearchRight(input []string, start complex128, keyword string) bool {
 			return false
 		}
 	}
+
 	return true
-}
-
-func wordSearchUpRight(input []string, start complex128, keyword string) bool {
-	return true
-}
-
-func wordSearchDownright(input []string, start complex128, keyword string) bool {
-	return false
-}
-
-func wordSearchDown(input []string, start complex128, keyword string) bool {
-	return false
-}
-
-func wordSearchDownLeft(input []string, start complex128, keyword string) bool {
-	return false
-}
-
-func wordSearchLeft(input []string, start complex128, keyword string) bool {
-	return false
 }
 
 func wordSearchUpLeft(input []string, start complex128, keyword string) bool {
-	return false
+	// bounds checking
+	if !checkBounds('U', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('L', start, len(keyword), complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	for idx, letter := range keyword {
+		if input[row(start)-idx][column(start)-idx] != byte(letter) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func wordCrossCheck(input []string, position complex128, keyword string) bool {
+	// add one to leglenth or bounds checking fails
+	legLength := int(math.Floor(float64(len(keyword))/2)) + 1
+	topMas := false
+	botMas := false
+
+	// bounds checking
+	if !checkBounds('U', position, legLength, complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('D', position, legLength, complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('L', position, legLength, complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+	if !checkBounds('R', position, legLength, complex(float64(len(input)), float64(len(input[0])))) {
+		return false
+	}
+
+	if wordSearchDownRight(input, position-1-1i, keyword) || wordSearchUpLeft(input, position+1+1i, keyword) {
+		topMas = true
+	}
+
+	if wordSearchUpRight(input, position+1-1i, keyword) || wordSearchDownLeft(input, position-1+1i, keyword) {
+		botMas = true
+	}
+
+	if topMas && botMas {
+		return true
+	} else {
+		return false
+	}
 }
 
 func (p Day04) PartA(lines []string) any {
@@ -85,7 +234,7 @@ func (p Day04) PartA(lines []string) any {
 					hits++
 				}
 
-				if wordSearchDownright(input, position, keyword) {
+				if wordSearchDownRight(input, position, keyword) {
 					hits++
 				}
 
@@ -108,9 +257,24 @@ func (p Day04) PartA(lines []string) any {
 		}
 	}
 
-	return "implement_me"
+	return hits
 }
 
 func (p Day04) PartB(lines []string) any {
-	return "implement_me"
+	input := lines[:len(lines)-1]
+	hits := 0
+	keyword := "MAS"
+	for row, data := range input {
+		for column, letter := range data {
+			if letter == 'A' {
+				position := complex(float64(row), float64(column))
+				if wordCrossCheck(input, position, keyword){
+					hits++
+				}
+			}
+		}
+	}
+
+	return hits
+
 }
