b056c2c829945c54d94569bb67610aba76232dc8
delve <delve202@gmail.com>
2024-12-13 07:09:02 +0000

    202413 wip

--- pkg/year2024/year2024_test.go
+++ pkg/year2024/year2024_test.go
@@ -211,3 +211,20 @@ func Benchmark2024Day12(b *testing.B) {
 		}
 	})
 }
+func Benchmark2024Day13(b *testing.B) {
+	Init()
+	input := aoc.TestInput(2024, 13)
+	p := aoc.NewPuzzle(2024, 13)
+	b.Run("PartA", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartA(input)
+		}
+	})
+	b.Run("PartB", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartB(input)
+		}
+	})
+}
